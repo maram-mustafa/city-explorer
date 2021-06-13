412320b3b3c67dea6bb2da6c26f27ca191472daf
maram-mustafa <maram.w.mustafa@gmail.com>
2021-06-13 21:46:49 +0300

    solve lab6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    locationData: "",
+    error: "",
+  };
+
+  getLocInfo = async (event) => {
+    event.preventDefault();
+    let cityName = event.target.cityName.value;
+    console.log(event);
+
+    try {
+      let data = await axios.get(
+        "https://eu1.locationiq.com/v1/search.php?key=pk.98e8463eaa916498acdf6dae4b49820e	&q=Amman&format=json"
+      );
+      this.setState({ locationData: data.data[0], error: "" });
+    } catch {
+      this.setState({ error: "ERROR!!!" });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" name="cityName"></input>
+          <input type="submit" name="submit" onSubmit={this.getLocInfo}></input>
+        </form>
+
+        {this.state.error ? (
+          <p> Error: {this.state.error}</p>
+        ) : (
+          this.state.locationData && (
+            <>
+              <p> Location: {this.state.locationData.display_name}</p>
+              <p>Latitude : {this.state.locationData.let}</p>
+              <p>Longitude: {this.state.locationData.lon}</p>
+            </>
+          )
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Component/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <header className="jumbotron text-center">
+          <h1 className="header"> City Explorer</h1>
+        </header>
+      </div>
+    );
+  }
+}
+export default Header;
+/*********************************/
