452e8ebf6ba8200345e49a5dcc5be69cee2cb89b
maram-mustafa <maram.w.mustafa@gmail.com>
2021-06-17 01:44:04 +0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Movies from "./Component/Movies";
-// import Weather from "./Component/Weather";
+import Weather from "./Component/Weather";
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Weather />
         <Movies />
       </div>
     );
